be90a4970f7958c5bee92a05a5d9ee0e1a5006d7
dev13 <dev13@codecraftdev.com>
2023-08-04 14:49:56 -0500

    .

---
+++ src/controllers/category.js
@@ -0,0 +1,36 @@
+const Category = require('../models/Category');
+const {v4: uuid} = require('uuid');
+
+const getCategory = async (req, res) => {
+  try {
+    const category = await Category.find();
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(500).send('No se encontro ninguna categoría :c')
+  }
+}
+
+const createCategory = async (req, res) => {
+  try {
+    const categoryData = req.body;
+    categoryData._id = uuid();
+    categoryData.key = "S-" + key.trim().slice(0, 3).toUpperCase();
+    const newCategory = await new Category(categoryData).save();
+    res.status(201).json(newCategory);
+  } catch (error) {
+    res.status(500).send('No se puso registrar el libro :c')
+  }
+}
+
+const updateCategory = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(500).send('No se pudo actualizar el libro :c')
+  }
+}
+
+module.exports = {
+  getCategory,
+  createCategory
+}
